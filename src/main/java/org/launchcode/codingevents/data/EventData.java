1e759e90f22346886bb5e2b37e1e65888656d4c2
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-21 21:53:07 -0500

    Created EventData which will store data in a Map<Integer, Event>

---
+++ src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -0,0 +1,21 @@
+package org.launchcode.codingevents.data;
+
+import org.launchcode.codingevents.models.Event;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventData {
+
+    // need a place to put events
+    private static Map<Integer, Event> events = new HashMap<>();
+
+    // get all events
+
+    // get a single event
+
+    // add an event
+
+    // remove an event
+
+}
